f2fe4affc874d28c8534852e0922196bdf7f56df
Tushar <tusharvamanamdoskar@gmail.com>
2022-06-19 15:27:53 +0530

    1

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import pymongo
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@cross_origin()
+@app.route('/addtask')
+def addtask():
+    return {"status":200}
+
+@cross_origin()
+@app.route('/getCards',methods=['GET'])
+def getCards():
+    
+    client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.wonbr.mongodb.net/?retryWrites=true&w=majority")
+    db = client.MentalHacks
+    col = db["cards"]
+    retDict = {"res":[]}
+    for x in col.find():
+        x.pop("_id")
+        retDict["res"].append(x)
+        print(x)
+
+
+    return retDict
+
+
+app.run(debug=True)
